5c1688015dfb7ff181e0c41bce956e8d5b643bb3
Bill29200 <nabil.kadjouh@gmail.com>
2024-05-15 11:10:18 +0200

    02/ Components : Admin, Server, Client, Home and Menu added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminComponent } from './components/admin/admin.component';
+import { ServerComponent } from './components/server/server.component';
+import { ClientComponent } from './components/client/client.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ServerComponent,
+    ClientComponent,
+    HomeComponent,
+    MenuComponent,
+
   ],
   imports: [
     BrowserModule,
