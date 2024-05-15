d1bb312af823193671ba49a724b9811f9fd684cc
Bill29200 <nabil.kadjouh@gmail.com>
2024-05-16 00:55:39 +0200

    03/ Navigate system installed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ClientComponent } from './components/client/client.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServerComponent } from './components/server/server.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'admin', component:AdminComponent},
+  {path:'server', component:ServerComponent},
+  {path:'client', component:ClientComponent},
+  {path:'about', component:AboutComponent},
+  {path:'', component:HomeComponent},
+  {path:'**', component:HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
 import { AdminComponent } from './components/admin/admin.component';
-import { ServerComponent } from './components/server/server.component';
 import { ClientComponent } from './components/client/client.component';
 import { HomeComponent } from './components/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
-
+import { ServerComponent } from './components/server/server.component';
 
 @NgModule({
   declarations: [
@@ -19,11 +25,21 @@ import { MenuComponent } from './components/menu/menu.component';
     ClientComponent,
     HomeComponent,
     MenuComponent,
+    AboutComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet
   ],
   providers: [
     provideAnimationsAsync()
