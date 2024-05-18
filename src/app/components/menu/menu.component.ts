841c923ad00d19697218a8a16b336fd55847bfeb
Bill29200 <nabil.kadjouh@gmail.com>
2024-05-18 21:56:59 +0200

    logOut & username added

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 
+
+  constructor(public authService : AuthService, private router: Router){}
+  //----------------------------
+logout() {
+this.authService.logout();
+this.router.navigateByUrl('/login');
+}
+//------------------------------------------
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,12 +4,18 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+
 //----------------------------------------------------------------
   public users :any ={
    admin:{password:'1234', roles:['ADMIN','SERVER','CLIENT']},
    server:{password:'1234', roles:['SERVER']},
    client:{password:'1234', roles:['CLIENT']}
   }
+
+  public username :any;
+  public isAuthenticated : boolean = false;
+  public roles : string []=[];
+
 //----------------------------------------------------------------
   constructor() { }
 //----------------------------------------------------------------
@@ -17,11 +23,23 @@ export class AuthService {
   {
     if (this.users[username] && this.users[username]['password']==password)
       {
+        this.username = username;
+        this.isAuthenticated = true;
+        this.roles = this.users[username]['roles'];
         return true;
       } else
       {
+        this.username='';
+        this.isAuthenticated= false;
+        this.roles = [];
+
         return false;
       }
   }
   //----------------------------------------------------------------
+  logout() {
+    this.isAuthenticated=false;
+    this.username = undefined;
+    this.roles=[];
+  }
 }
